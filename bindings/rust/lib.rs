1e4427a1251be7abce5154d1f6ce184eadeeb0ca
Pedro Ribeiro <p.m.ribeiro16@gmail.com>
2026-05-13 10:32:08 +0100

    update rust FFI bindings to use the correct parser symbol

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -26,13 +26,13 @@
 use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_rust() -> *const ();
+    fn tree_sitter_verus() -> *const ();
 }
 
 /// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
 ///
 /// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_rust) };
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_verus) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
